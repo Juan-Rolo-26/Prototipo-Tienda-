8c177919c2f288c92a77f4b2a9c97111ebe72572
franciscoalvarez <franchelaa1@gmail.com>
2026-02-23 17:36:01 +0000

    1

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-const app = require("./src/server");
-module.exports = app;
+process.on("uncaughtException", (err) => {
+  console.error("💥 UNCAUGHT EXCEPTION (root server.js):", err?.stack || err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("💥 UNHANDLED REJECTION (root server.js):", reason?.stack || reason);
+  process.exit(1);
+});
+
+console.log("✅ Root entry server.js starting...");
+
+try {
+  require("./src/server.js");
+} catch (err) {
+  console.error("🔥 FAILED TO REQUIRE ./src/server.js:", err?.stack || err);
+  process.exit(1);
+}
