fe5a073f1b0fcb40e03321daac49f7b4a2a929df
franciscoalvarez <franchelaa1@gmail.com>
2026-02-23 17:40:40 +0000

    3

--- src/server.js
+++ src/server.js
@@ -7,14 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("EXPRESS MODE OK");
-});
-
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
 });
 
 app.use("/api/test", testRoutes);
+
+const path = require("path");
+
+const FRONTEND_DIST = path.join(__dirname, "..", "frontend", "dist");
+
+app.use(express.static(FRONTEND_DIST));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(FRONTEND_DIST, "index.html"));
+});
 
 module.exports = app;
